1ef6c3e2366fea7d69d5fe0814d1a067d9a830af
Kaitlyn Wyland <k1wylandpa@gmail.com>
2025-05-12 21:59:01 -0400

    Update respawn script to work

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -6,29 +6,38 @@ public class Respawn : MonoBehaviour
 {
     public Vector3 respawnPoint { get; private set; }
     FirstPersonController player;
+    CharacterController characterController;
 
     void Start()
     {
         player = FindObjectOfType<FirstPersonController>();
-        respawnPoint = transform.position;
+        characterController = GetComponent<CharacterController>();
+        respawnPoint = characterController.transform.position;
     }
-
 
     void Update()
     {
-        // if (player.Grounded)
-        // {
-        //     respawnPoint = transform.position;
-        //     Debug.Log(respawnPoint);
-        // }
+        if (player != null && player.Grounded)
+        {
+            Vector3 currentPosition = characterController.transform.position;
+            // Only update if moved more than 0.1 units
+            if (Vector3.Distance(currentPosition, respawnPoint) > 0.1f) 
+            {
+                respawnPoint = currentPosition;
+            }
+        }
     }
 
     protected void OnTriggerEnter(Collider other)
     {
-        // if (other.gameObject.tag == "FallDetector")
-        // {
-        //     Debug.Log("Fall");
-        //     player.transform.position = respawnPoint;
-        // }
+        if (other.CompareTag("FallDetector"))
+        {
+            // Disable the character controller temporarily
+            characterController.enabled = false;
+            // Set the position
+            transform.position = respawnPoint;
+            // Re-enable the character controller
+            characterController.enabled = true;
+        }
     }
 }
